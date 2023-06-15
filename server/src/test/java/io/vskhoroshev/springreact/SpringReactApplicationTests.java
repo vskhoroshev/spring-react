1613b0a3403eb781f17eb5b41b0f6cc5ff552b18
Vadim Khoroshev <vs.khoroshev@gmail.com>
2023-06-15 16:56:08 +0300

    init

---
+++ server/src/test/java/io/vskhoroshev/springreact/SpringReactApplicationTests.java
@@ -0,0 +1,12 @@
+package io.vskhoroshev.springreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
